b62e24f0146fddb867b1327e4ff384d1bde6bbbc
finnporter <iamfinnporter@gmail.com>
2017-07-25 20:38:02 +0100

    park can remove dino by type

--- park.js
+++ park.js
@@ -6,4 +6,9 @@ Park.prototype.add = function(dinosaur) {
   this.population.push(dinosaur);
 };
 
+Park.prototype.remove = function(dinosaurType) {
+  var dinoToRemove = this.population.indexOf(dinosaurType);
+    this.population.splice(dinoToRemove, 1);
+};
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -6,10 +6,12 @@ describe("Park", function() {
 
  var park;
  var dinosaur1;
+ var dinosaur1;
 
  beforeEach(function() {
   park = new Park();
   dinosaur1 = new Dinosaur("Tyrannosaurus");
+  dinosaur2 = new Dinosaur("Ceratops");
 });
 
  it("should start empty", function() {
@@ -18,6 +20,13 @@ describe("Park", function() {
 
  it("should be able to add dinosaurs", function() {
   park.add(dinosaur1);
+  assert.strictEqual(park.population.length, 1);
+ });
+
+ it("should be able to remove a dinosaur by type", function() {
+  park.add(dinosaur1);
+  park.add(dinosaur2);
+  park.remove("Ceratops")
   assert.strictEqual(park.population.length, 1);
  })
 
