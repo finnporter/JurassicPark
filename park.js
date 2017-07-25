d47e93a059333510c3f22dccfe8e209acd19d0a8
finnporter <iamfinnporter@gmail.com>
2017-07-25 19:43:12 +0100

    can add dinosaurs to park

--- park.js
+++ park.js
@@ -2,4 +2,8 @@ var Park = function() {
   this.population = [];
 };
 
+Park.prototype.add = function(dinosaur) {
+  this.population.push(dinosaur);
+};
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,21 +1,25 @@
 var assert = require("assert");
 var Park = require("../park");
+var Dinosaur = require("../dinosaur")
 
 describe("Park", function() {
 
  var park;
+ var dinosaur1;
 
  beforeEach(function() {
   park = new Park();
+  dinosaur1 = new Dinosaur("Tyrannosaurus");
 });
 
  it("should start empty", function() {
   assert.strictEqual(park.population.length, 0);
 });
 
- // it("should be able to add dinosaurs", function() {
- //  park.add("Ceratops")
- // })
+ it("should be able to add dinosaurs", function() {
+  park.add(dinosaur1);
+  assert.strictEqual(park.population.length, 1);
+ })
 
 
 });
